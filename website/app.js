b3f68cccc1d1c9d8dc54947dae50f34403fa8e41
Esraa-Mamdouh <esraa.ali9798@gmail.com>
2021-05-15 00:02:40 +0200

    fix: temperature bug fixed

--- website/app.js
+++ website/app.js
@@ -86,9 +86,8 @@ function generateListner(){
     console.log("zip= "+document.getElementById("zip").value)
     zipCode=document.getElementById("zip").value;
     if(zipCode== ""){
-        alert("please enter zip code")
-    }
-    else{
+        alert("please enter zip code")}
+   
     baseUrl = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${key}&units=metric`;
     //get value of feeling and set it to object
     projectData.content= document.getElementById("feelings").value;
@@ -110,7 +109,7 @@ function generateListner(){
     
     .then(
         getServerData('/updateUI')
-    )}
+    )
     })
 }
 
